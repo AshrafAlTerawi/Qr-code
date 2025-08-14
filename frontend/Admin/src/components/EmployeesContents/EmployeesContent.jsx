e689d4972df10604a08685514047e31f2e34af37
nour <nour3lwan99@gmail.com>
2025-08-14 16:13:21 +0300

    employee content get data

--- frontend/Admin/src/components/EmployeesContents/EmployeesContent.jsx
+++ frontend/Admin/src/components/EmployeesContents/EmployeesContent.jsx
@@ -1,12 +1,22 @@
 //css import
 import "./EmployeesContent.css";
 
+import axios from "axios";
 import React from "react";
+
+//import userContext
+import { useUsers } from "../../Contexts/UserContext";
 
 //react icon import
 import { FiUserPlus } from "react-icons/fi";
 
-const EmployeesContent = ({ employees }) => {
+
+
+
+const EmployeesContent = () => {
+  // get data from the context
+  const { Users } = useUsers(); // Users => array contains all the user i got them from the back
+
   return (
     <div className="employees-content">
       <div className="employees-header">
@@ -21,24 +31,18 @@ const EmployeesContent = ({ employees }) => {
           <thead>
             <tr>
               <th>ID</th>
-              <th>Name</th>
+              <th>Full Name</th>
               <th>Email</th>
-              <th>Position</th>
-              <th>Department</th>
               <th>Join Date</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
-            {employees.map((emp) => (
+            {Users.map((emp) => (
               <tr key={emp.id}>
                 <td>{emp.id}</td>
-                <td>{emp.name}</td>
-                <td>
-                  {emp.name.replace(/\s+/g, "").toLowerCase()}@company.com
-                </td>
-                <td>{emp.position}</td>
-                <td>IT</td>
+                <td>{emp.full_name}</td>
+                <td>{emp.email}</td>
                 <td>15/03/2023</td>
                 <td>
                   <button className="edit-btn">Edit</button>
